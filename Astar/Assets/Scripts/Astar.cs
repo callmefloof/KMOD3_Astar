d8efa976ff1138c678f4454ff29120e639ef2a55
Joëlle Ubink <joelle.ubink@student.hku.nl>
2024-11-18 20:48:22 +0100

    Working AStar, but does not respect walls yet

--- Astar/Assets/Scripts/Astar.cs
+++ Astar/Assets/Scripts/Astar.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
+using UnityEditor.Experimental.GraphView;
 
 public class Astar
 {
@@ -15,8 +16,115 @@ public class Astar
     /// <returns></returns>
     public List<Vector2Int> FindPathToTarget(Vector2Int startPos, Vector2Int endPos, Cell[,] grid)
     {
+        
+        
+        List<Node> openSet = new List<Node>();
+        List<Node> closedSet = new List<Node>();
+
+        float distStartToEnd = Vector2Int.Distance(startPos, endPos);
+        int hScoreStart = Mathf.RoundToInt(distStartToEnd*10);
+        Node startNode = new Node(startPos, null, 0, hScoreStart);
+        openSet.Add(startNode);
+        
+        
+        while(openSet.Count != 0)
+        {
+            Node current = openSet.OrderBy(node => node.FScore).First();
+            if(current.position == endPos)
+            {
+                return ReconstructPath(current);
+            }
+            Vector2Int currentPos = current.position;
+            openSet.Remove(current);
+            closedSet.Add(current);
+            
+            List<Vector2Int> directions = new List<Vector2Int>
+            {
+                Vector2Int.left,
+                Vector2Int.right,
+                Vector2Int.up,
+                Vector2Int.down,
+                Vector2Int.left + Vector2Int.up,
+                Vector2Int.right + Vector2Int.up,
+                Vector2Int.left + Vector2Int.down,
+                Vector2Int.right + Vector2Int.down
+            };
+
+            foreach(Vector2Int direction in directions)
+            {
+                Vector2Int neighborPos = currentPos + direction;
+                if(closedSet.Any(node => node.position == neighborPos))
+                {
+                    continue;
+                }
+                Node neighbor = openSet.Find(n => n.position == neighborPos);
+
+                float distCtoN = Vector2Int.Distance(current.position, neighborPos) *10;
+                int tentativeGScore = (int)(current.GScore + distCtoN);
+                if(neighbor == null || tentativeGScore < neighbor.GScore)
+                {
+                    
+                    if(neighbor == null)
+                    {
+                        int distNtoE = (int)Vector2Int.Distance(current.position, neighborPos) *10;
+                        neighbor = new Node(neighborPos, current, tentativeGScore,distNtoE);
+                    }
+                    else
+                    {
+                        neighbor.GScore = tentativeGScore;
+                        neighbor.parent = current;
+                    }
+                    if(!openSet.Contains(neighbor))
+                    {
+                        openSet.Add(neighbor);
+                    }
+                }
+                
+            }
+            
+            
+        }
+
         return null;
     }
+
+
+    private List<Vector2Int> ReconstructPath(Node lastNode)
+    {
+        List<Vector2Int> totalPath = new List<Vector2Int>();
+        Node currentNode = lastNode;
+        while(currentNode != null)
+        {
+            if(totalPath.Count == 0)
+            {
+                totalPath.Add(currentNode.position);
+            }
+            else
+            {
+                totalPath = totalPath.Prepend(currentNode.position).ToList();
+            }
+            currentNode = currentNode.parent;
+
+        }
+        return totalPath;
+    }
+
+    // private Dictionary<Vector2Int, Node> GenerateNodes(Cell[,] grid)
+    // {
+        
+    //     Dictionary<Vector2Int,Node> nodes = new Dictionary<Vector2Int, Node>();
+    //     for(int x = 0; x < grid.GetLength(0); x++)
+    //     {
+    //         for(int y = 0; y < grid.GetLength(1); y++)
+    //         {
+    //             int gCost = 0;     
+    
+    //             Node node = new Node(grid[x,y].gridPosition, null, gCost, 0);
+    //             nodes.Add(grid[x,y].gridPosition, node);
+    //         }
+    //     }
+    //     return nodes;
+    // }
 
     /// <summary>
     /// This is the Node class you can use this class to store calculated FScores for the cells of the grid, you can leave this as it is
